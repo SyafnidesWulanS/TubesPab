fbc20e7337bf7381bedb025bd4fadc69df4da1c3
SyafnidesWulanS <80112538+SyafnidesWulanS@users.noreply.github.com>
2022-12-11 17:14:23 +0700

    11/12/2022 projek

---
+++ app/src/main/java/com/example/tubes/halamanVideo.kt
@@ -0,0 +1,11 @@
+package com.example.tubes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class halamanVideo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_video)
+    }
+}
